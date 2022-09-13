34e7cfb1f9356350d88bbe9c0639dd9a67f66649
Tram La <latb@uci.edu>
2022-09-12 21:36:59 -0700

    create pop up for background selection

--- src/components/Background/BackgroundIcon.js
+++ src/components/Background/BackgroundIcon.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { FormControl } from 'react-bootstrap';
+import Background from './Background';
 
-const BackgroundIcon = ({background}) => {
+const selectBackground = (background) => {
+    console.log(background)
+}
+
+const BackgroundIcon = ({ allBackgrounds ,background }) => {
+
     return (
         <span className='background-info'>
-            <img className='icon left' src={background.path} alt={background.description}/>
+            <img className='icon left' src={background.path} 
+                alt={background.description}
+                onClick={ () => selectBackground(background) }
+                />
             <h5 className='right'> {background.description} </h5>
         </span>
     )
--- src/components/Background/SelectBackground.js
+++ src/components/Background/SelectBackground.js
@@ -3,18 +3,24 @@ import './Background.css'
 
 const SelectBackground = ({allBackgrounds, closeSelect}) => {
   return (
-    <div className="popup">
-      <h5 className='title'> Select a Background</h5>
-      <div className='close-button'>
-        <button onClick={closeSelect}> X </button>
-      </div>
-      <ul>
-        {allBackgrounds.map((task) => (
-          <li key={task.id}>
-            <BackgroundIcon background={task}/>
-          </li>
-        ))}
-      </ul>
+      <div className="popup-inner">
+        <div className="popup-header">
+          <h5 className='title'> Select a Background</h5>
+          <div className='close-button'>
+            <button onClick={closeSelect}> X </button>
+          </div>
+        </div>
+        <div className="popup-list-outer">
+          <div className="popup-list">
+            <ul>
+              {allBackgrounds.map((task) => (
+                <li key={task.id}>
+                  <BackgroundIcon allBackgrounds={allBackgrounds} background={task}/>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
   )
 }

0a97c1e845c46c4828703d1868a5fbd517479633
Tram La <latb@uci.edu>
2023-02-17 20:47:46 -0800

    fix scroll bug of to-do list

--- src/components/ToDoList/Tasks.js
+++ src/components/ToDoList/Tasks.js
@@ -18,11 +18,13 @@ const Tasks = ({ tasks, onDelete, addTask, toggleToDo }) => {
                 {showAddTask && <AddTask addTask={addTask} closeAdd={() => toggleAddTask(!showAddTask)}/>}
             </div>
             <div className="task-list-outer">
+                <div className="task-list-inner">
                 {tasks.length > 0 ? (
-                <ul style={{paddingRight:"1rem"}}>
-                    {tasks.map((task) => (<Task key={task.id} task={task} 
-                    onDelete={onDelete}></Task>))} 
-                </ul>) : "No current task to display ... Z z z..."}      
+                    <ul style={{paddingRight:"1rem"}}>
+                        {tasks.map((task) => (<Task key={task.id} task={task} 
+                        onDelete={onDelete}></Task>))} 
+                    </ul>) : "No current task to display ... Z z z..."}      
+                </div>
             </div>
         </div>
     </div>
